fdbd54dadfaa776b7201ed955246beebbd24596f
Partha Kuri <pskuri@gmail.com>
2021-10-11 22:35:36 -0400

    Adding a new model

--- testapp01/models.py
+++ testapp01/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import IntegerField
 
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
@@ -6,3 +7,10 @@ class Person(models.Model):
     
     def __unicode__(self):
       return self.first_name + ' ' + self.last_name
+
+class Customer(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.TextField
+    dob = models.DateField
+    age = models.IntegerField
+    extra = models.SlugField
